0b7d8edc97f41d8e0a53eaafc6cf741f80942e93
veritasdol <veritas.dol@gmail.com>
2025-07-03 07:41:05 +0200

    refactor: encapsulate request handling in AllegroAuth class methods

--- src/allegro_auth/client.py
+++ src/allegro_auth/client.py
@@ -5,62 +5,68 @@ from .token_manager import read_tokens, write_tokens, is_token_valid
 
 requests.packages.urllib3.disable_warnings()
 
-def send_request(method: str, url: str, data: dict):
-    try:
-        response = requests.request(
-            method=method,
-            url=url,
-            data=data,
-            auth=(CLIENT_ID, CLIENT_SECRET),
-            verify=True,
-            allow_redirects=False
-        )
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        raise SystemExit(f"Request failed: {e}")
+class AllegroAuth:
+    def __init__(self, client_id: str, client_secret:str, redirect_uri: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
 
-def get_authorization_code():
-    params = urlencode({
-        "response_type": "code",
-        "client_id": CLIENT_ID,
-        "redirect_uri": REDIRECT_URI
-    })
-    print(f"Open this URL and authorize the app:\n\n{AUTH_URL}?{params}\n")
-    return input("Enter the code from URL: ")
+    
 
-def get_token_by_code(code: str):
-    return send_request("POST", TOKEN_URL, {
-        "grant_type": "authorization_code",
-        "code": code,
-        "redirect_uri": REDIRECT_URI
-    })
+    def __send_request(self, method: str, data: dict):
+        try:
+            response = requests.request(
+                method=method,
+                url=TOKEN_URL,
+                data=data,
+                auth=(self.client_id, self.client_secret),
+                verify=True,
+                allow_redirects=False
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            raise SystemExit(f"Request failed: {e}")
 
-def refresh_token(refresh_token: str):
-    return send_request("POST", TOKEN_URL, {
-        "grant_type": "refresh_token",
-        "refresh_token": refresh_token,
-        "redirect_uri": REDIRECT_URI
-    })
+    def __get_authorization_code(self):
+        params = urlencode({
+            "response_type": "code",
+            "client_id": self.client_id,
+            "redirect_uri": self.redirect_uri
+        })
+        print(f"Open this URL and authorize the app:\n\n{AUTH_URL}?{params}\n")
+        return input("Enter the code from URL: ")
 
-def authenticate(client_secret: str, client_id: str, redirect_uri: str) -> str:
-    global CLIENT_ID
-    global CLIENT_SECRET
-    global REDIRECT_URI
-    CLIENT_ID = client_id
-    CLIENT_SECRET = client_secret
-    REDIRECT_URI = redirect_uri
+    def __get_token_by_code(self, code: str):
+        return self.__send_request(
+            "POST", 
+            {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": self.redirect_uri
+            })
 
-    token_data = read_tokens()
+    def __refresh_token(self, refresh_token: str):
+        return self.__send_request(
+            "POST", 
+            {
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "redirect_uri": self.redirect_uri
+            })
 
-    if token_data and is_token_valid(token_data.expires_in, token_data.date):
-        return token_data.access_token
+    def authenticate(self) -> str:
 
-    if token_data:
-        new_tokens = refresh_token(token_data.refresh_token)
-    else:
-        code = get_authorization_code()
-        new_tokens = get_token_by_code(code)
+        token_data = read_tokens()
 
-    write_tokens(new_tokens)
-    return new_tokens["access_token"]
+        if token_data and is_token_valid(token_data.expires_in, token_data.date):
+            return token_data.access_token
+
+        if token_data:
+            new_tokens = self.__refresh_token(token_data.refresh_token)
+        else:
+            code = self.__get_authorization_code()
+            new_tokens = self.__get_token_by_code(code)
+
+        write_tokens(new_tokens)
+        return new_tokens["access_token"]
